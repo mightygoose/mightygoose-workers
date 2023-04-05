865281a15a5a5ade1ce612a08a235f644dbec5e9
Aleksandr Zhukov <aleksandrzhukov@00008809s-MacBook-Pro.local>
2023-04-05 18:10:12 +0200

    fix REDIS_MAX_CONNECTION_RETRIES variable value

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   REDIS_URL: process.env['REDIS_URL'],
-  REDIS_MAX_CONNECTION_RETRIES: process.env['REDIS_URL'] || 10,
+  REDIS_MAX_CONNECTION_RETRIES: process.env['REDIS_MAX_CONNECTION_RETRIES'] || 10,
   PROCESS_ITEM_JOB: 'process-item',
   ITEMS_CHANNEL: process.env['PUB_SUB_CHANNEL'] || 'items',
   RESTORING_DELAY: process.env['RESTORING_DELAY'] || 5000,
